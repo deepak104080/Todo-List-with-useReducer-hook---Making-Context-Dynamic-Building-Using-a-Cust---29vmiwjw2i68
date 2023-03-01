b466d02c6aea631cd419f4a8361be113c713c922
deepak104080 <deepak104080@gmail.com>
2023-03-01 23:02:52 +0530

    assg to do

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,7 +1,48 @@
 const todoReducer = (state, action) => {
+    const {type, payload} = action;
     switch (action.type) {
-
+        case 'ADDTODO':
+            let obj = {
+                title: payload,
+                id: Date.now()
+            }
+            return [...state, obj]
+        case 'DELETETODO':
+            let retArr = state.filter((item) => {
+                return item.id !== payload
+                })
+            return [...retArr]
+        default:
+            return state
     }
 }
 
 export { todoReducer }
+
+
+// [
+//     {
+//         title: 'Go to market',
+//         id: 123399988
+//     },
+//     {
+//         title: 'aaa',
+//         id: 12311
+//     },
+//     {
+//         title: 'aaa',
+//         id: 12311
+//     },
+//     {
+//         title: 'aaa',
+//         id: 12311
+//     },
+//     {
+//         title: 'aaa',
+//         id: 1234
+//     },
+//     {
+//         title: 'Go to market',
+//         id: 12388
+//     }
+// ]
